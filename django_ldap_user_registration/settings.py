6103129d1a843789388ac0454bcdcf0e88f0932f
Ronald Osure <rosure@kenet.or.ke>
2018-06-26 14:44:39 +0300

    add local settings, add anymail

--- django_ldap_user_registration/settings.py
+++ django_ldap_user_registration/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Import local settings
+from django_ldap_user_registration.local_settings import *
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'anymail',
 ]
 
 MIDDLEWARE = [
