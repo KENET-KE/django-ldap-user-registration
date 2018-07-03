d9bd3e86f136fd4b6626d812dc92f96e13c95fc6
Ronald Osure <rosure@kenet.or.ke>
2018-07-03 14:53:16 +0300

    introduce ldap, add username validation to registration form

--- user/forms.py
+++ user/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.urls import reverse
+from django.core.validators import RegexValidator
 
 from crispy_forms.bootstrap import Field, InlineRadios, TabHolder, Tab
 from crispy_forms.helper import FormHelper
@@ -8,18 +8,28 @@ from crispy_forms.layout import Submit, Layout, Div, Fieldset
 
 class UserRegisterForm(forms.Form):
     title_choices = (('Mr.', 'Mr'), ('Ms', 'Ms',), ('Dr.', 'Dr.',),)
+    country_choices = (('Kenya', 'Kenya'),)
+    organization_choices = (('kenet', 'KENET'),('other', 'Select other'),)
     first_name = forms.CharField(required=True, max_length=255)
     last_name = forms.CharField(required=True, max_length=255)
     email = forms.EmailField(required=True)
     title = forms.ChoiceField(required=False, choices=title_choices)
     designation = forms.CharField(max_length=200)
-    organization = forms.ChoiceField(required=True, choices=())
-    username = forms.CharField(required=True, max_length=100)#, validators=)
+    organization = forms.ChoiceField(required=True, choices=organization_choices)
+    username = forms.CharField(required=True, max_length=30, help_text='Choose a memorable name e.g jdoe',
+                               validators=[
+                                   RegexValidator(
+                                       regex='^[a-zA-Z0-9_]*$',
+                                       message='Username must be Alphanumeric. Allowed chars [a-zA-Z0-9_]',
+                                       code='invalid_username'
+                                   ),
+                               ]
+                               )
     password = forms.CharField(widget=forms.PasswordInput)
     password1 = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')
     phone = forms.CharField(required=True, max_length=200)
     address = forms.CharField(max_length=1000, widget=forms.Textarea())
-    country = forms.ChoiceField(required=True, choices=())  # Set to default to your country
+    country = forms.ChoiceField(required=True, choices=country_choices)  # Set to default to your country
     gender = forms.TypedChoiceField(
         choices=((0, "Male"), (1, "Female"),),
         coerce=lambda x: bool(int(x)),
--- user/views.py
+++ user/views.py
@@ -1,4 +1,8 @@
+import ldap
+
 from django.views import generic
+from django.http.response import HttpResponse
+from django.conf import settings
 
 from user.forms import UserRegisterForm
 
@@ -11,4 +15,30 @@ class IndexView(generic.TemplateView):
 class RegisterView(generic.FormView):
     template_name = 'user/register.html'
     form_class = UserRegisterForm
-    success_url = ''
+    success_url = '/abc/'
+    def ldap_connect(self):
+        self.con = ldap.initialize(settings.LDAP_PROTO + '://' + settings.LDAP_HOST + ':' + settings.LDAP_PORT)
+        self.con.simple_bind_s(settings.LDAP_BIND_DN, settings.LDAP_BIND_DN_CREDENTIAL)
+
+    def form_valid(self, form):
+        self.ldap_connect()
+        query = "(uid=admin)"
+        result = self.con.search_s('dc=zion,dc=co,dc=ke', ldap.SCOPE_SUBTREE, query)
+        data = form.cleaned_data
+        full_name = data.get('username') + ' ' + data.get('last_name')
+        dn = 'uid=' + data.get('username') + ',' + settings.LDAP_BASE_DN
+        modlist = {
+            "objectClass": ["inetOrgPerson", "posixAccount", "shadowAccount"],
+            "uid": [data.get('username')],
+            "sn": [data.get('last_name')],
+            "givenName": [data.get('first_name')],
+            "cn": [full_name],
+            "displayName": [full_name],
+            "uidNumber": ["1001"],
+            "gidNumber": [settings.LDAP_GID],
+            "loginShell": ["/bin/bash"],
+            "homeDirectory": ["/home/users/" + data.get('username')]
+        }
+
+        result = self.con.add_s(dn, ldap.modlist.addModlist(modlist))
+        return HttpResponse(result)
