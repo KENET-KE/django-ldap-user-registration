e6fc24170d35fd56dd8f8c8bc7d09629fff3e009
Ronald Osure <rosure@kenet.or.ke>
2018-07-17 14:35:45 +0300

    add service provider to context pre-processor

--- user/context_processors.py
+++ user/context_processors.py
@@ -6,5 +6,7 @@ def site(request):
     return {
         'SITE_BASE_URL': settings.SITE_BASE_URL,
         'IDP_NAME': settings.IDP_NAME,
-        'IDP_LOGO': settings.IDP_LOGO
+        'IDP_LOGO': settings.IDP_LOGO,
+        'SERVICE_PROVIDER': settings.SERVICE_PROVIDER,
+        'SERVICE_PROVIDER_URL': settings.SERVICE_PROVIDER_URL
     }
